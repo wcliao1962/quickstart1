59f7cb5865bb70b5ae442da960d198389a70d2a8
wen-chung <wcliao1962@gmail.com>
2017-01-26 16:11:51 +0800

    驗證表單輸入

--- routes/api.php
+++ routes/api.php
@@ -32,8 +32,19 @@ Route::group(['middleware' => 'web'], function () {
      * 增加新的任務
      */
     Route::post('/task', function (Request $request) {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        // 建立該任務...
     });
+
 
     /**
      * 刪除任務
