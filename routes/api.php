0fe882e06c79f4cbc48b8fef562df46b882a2cc8
wen-chung <wcliao1962@gmail.com>
2017-01-26 12:34:10 +0800

    建置路由

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Task;
 use Illuminate\Http\Request;
 
 /*
@@ -13,6 +14,30 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['middleware' => 'web'], function () {
+
+    /**
+     * 顯示所有任務
+     */
+    Route::get('/', function () {
+        //
+    });
+
+    /**
+     * 增加新的任務
+     */
+    Route::post('/task', function (Request $request) {
+        //
+    });
+
+    /**
+     * 刪除任務
+     */
+    Route::delete('/task/{task}', function (Task $task) {
+        //
+    });
 });
