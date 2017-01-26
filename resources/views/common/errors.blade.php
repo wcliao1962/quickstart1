e66f4d97870ca8d5ac2e4349e4c9d19f2c43ff82
wen-chung <wcliao1962@gmail.com>
2017-01-26 16:23:17 +0800

    error message

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: wen
- * Date: 2017/1/26
- * Time: 下午 03:54
- */
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
