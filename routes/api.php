868fc39d42bbe57fcea73d4a4346e311b67b4124
wen-chung <wcliao1962@gmail.com>
2017-01-27 13:01:10 +0800

    create TaskController & its related routes

--- routes/api.php
+++ routes/api.php
@@ -23,49 +23,53 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'web'], function () {
 
-    /**
-     * 顯示所有任務
-     */
-    Route::get('/', function () {
-        //
-        $tasks = Task::orderBy('created_at', 'asc')->get();
+//    /**
+//     * 顯示所有任務
+//     */
+//    Route::get('/', function () {
+//        //
+//        $tasks = Task::orderBy('created_at', 'asc')->get();
+//
+//        return view('tasks', [
+//            'tasks' => $tasks
+//        ]);
+//
+//    });
+//
+//    /**
+//     * 增加新的任務
+//     */
+//    Route::post('/task', function (Request $request) {
+//        $validator = Validator::make($request->all(), [
+//            'name' => 'required|max:255',
+//        ]);
+//
+//        if ($validator->fails()) {
+//            return redirect('/api')
+//                ->withInput()
+//                ->withErrors($validator);
+//        }
+//
+//        // 建立該任務...
+//        $task = new Task;
+//        $task->name = $request->name;
+//        $task->save();
+//
+//        return redirect('/api');
+//    });
+//
+//
+//    /**
+//     * 刪除任務
+//     */
+//    Route::delete('/task/{id}', function ($id) {
+//        Task::findOrFail($id)->delete();
+//
+//        return redirect('/api');
+//
+//    });
 
-        return view('tasks', [
-            'tasks' => $tasks
-        ]);
-
-    });
-
-    /**
-     * 增加新的任務
-     */
-    Route::post('/task', function (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect('/api')
-                ->withInput()
-                ->withErrors($validator);
-        }
-
-        // 建立該任務...
-        $task = new Task;
-        $task->name = $request->name;
-        $task->save();
-
-        return redirect('/api');
-    });
-
-
-    /**
-     * 刪除任務
-     */
-    Route::delete('/task/{id}', function ($id) {
-        Task::findOrFail($id)->delete();
-
-        return redirect('/api');
-
-    });
+    Route::get('/tasks', 'TaskController@index');
+    Route::post('/task', 'TaskController@store');
+    Route::delete('/task/{task}', 'TaskController@destroy');
 });
