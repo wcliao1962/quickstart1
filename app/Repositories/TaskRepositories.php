9150f6fb861cfa30ddf194e776f9522be28b7983
wen-chung <wcliao1962@gmail.com>
2017-01-28 23:49:15 +0800

    定義一個 TaskRepository存放所有Task模型的資料存取邏輯

---
+++ app/Repositories/TaskRepositories.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wen
+ * Date: 2017/1/28
+ * Time: 下午 11:47
+ */
+namespace App\Repositories;
+
+use App\User;
+use App\Task;
+
+class TaskRepository
+{
+    /**
+     * 取得給定使用者的所有任務。
+     *
+     * @param  User  $user
+     * @return Collection
+     */
+    public function forUser(User $user)
+    {
+        return Task::where('user_id', $user->id)
+            ->orderBy('created_at', 'asc')
+            ->get();
+    }
+}
