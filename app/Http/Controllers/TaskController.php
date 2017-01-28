c14877b44ce9120a0d89573e70a056fbd6587eaa
wen-chung <wcliao1962@gmail.com>
2017-01-29 00:12:33 +0800

    顯示user所有task (利用TaskRepository)

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,20 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
     //
-    public function __construct()
+    /**
+     * 任務資源庫的實例。
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
+    /**
+     * 建立新的控制器實例。
+     *
+     * @param  TaskRepository  $tasks
+     * @return void
+     */
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+
+        $this->tasks = $tasks;
     }
 
+    /**
+     * 取得給定使用者的所有任務。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
     public function index(Request $request)
     {
-        return view('tasks.index');
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
     }
+
 
     public function store(Request $request)
     {
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -32,5 +32,45 @@
         </form>
     </div>
 
-    <!-- 代辦：目前任務 -->
+    <!-- 目前任務 -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                目前任務
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- 表頭 -->
+                    <thead>
+                    <th>Task</th>
+                    <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- 表身 -->
+                    <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <!-- 任務名稱 -->
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+
+                            <!-- 刪除按鈕 -->
+                            <td>
+                                <form action="/task/{{ $task->id }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <button>刪除任務</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
