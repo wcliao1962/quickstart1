de81a359d6ccd38fd8b12c9ad5c8d72e940f6a29
wen-chung liao <wcliao1962@gmail.com>
2017-01-29 16:21:13 +0800

    add a new route /home to view home/home.blade.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home.home');
+});
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 Route::delete('/task/{task}', 'TaskController@destroy');
